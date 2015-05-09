7478bbc0a0cd5fb196f042e66036f9ff2bf83f3a
matrim <ttroy50@gmail.com>
2015-05-10 00:50:53 +0100

    #3 autoconf cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -4,11 +4,12 @@ cd "$topsrcdir"
 make -k clean >/dev/null 2>&1
 make -k distclean >/dev/null 2>&1
 find . -name Makefile.in -exec rm -f {} \;
+find . -name Makefile.in -exec rm -f {} \;
 rm -rf \
 Makefile \
 Makefile.in \
 aclocal.m4 \
 autom4te.cache \
 config.log \
-config.status \
-configure
+config.status
+#configure
