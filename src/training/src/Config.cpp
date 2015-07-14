098dea0e5c4515e068eab419200fccd1a57f1561
ttroy50 <ttroy50@gmail.com>
2015-07-15 00:12:54 +0100

    #6 added some more attribute meters. Also fix for weird singleton issue

--- src/training/src/Config.cpp
+++ src/training/src/Config.cpp
@@ -16,24 +16,33 @@ using namespace std;
 std::unique_ptr<Config> Config::_instance;
 std::once_flag Config::_onceFlag;
 
+Config* Config::instance()
+{
+    try
+    {
+        std::call_once(_onceFlag,
+            [] {
+        if(!_instance) 
+                _instance.reset(new Config);
+            }
+        );
+    }
+    catch(std::runtime_error& e)
+    {
+        // not thread safe but this should be initialised alread in main thread
+        // Not sure why I was sometimes seeing exception from call_once. Need time to investigate
+        if(!_instance)
+            _instance.reset(new Config);
+    }
+
+    return _instance.get();
+}
+
 Config::Config() :
 	_protocol_database("protocol_model_db.yaml"),
 	_udp_flow_timeout(120)
 {
 
-}
-
-
-Config* Config::instance()
-{
-    std::call_once(_onceFlag,
-        [] {
-    if(!_instance) 
-            _instance.reset(new Config);
-        }
-    );
-
-    return _instance.get();
 }
 
 bool Config::init(const string& config_file)
